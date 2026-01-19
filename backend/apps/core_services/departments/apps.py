768e91f326d78ab97998ee84741e927566772804
hoahung2307 <angelnuyn@gmail.com>
2026-01-19 21:54:51 +0700

    Add departments/models.py and authentication/models.py

--- backend/apps/core_services/departments/apps.py
+++ backend/apps/core_services/departments/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DepartmentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'departments'
+    name = 'apps.core_services.departments'
