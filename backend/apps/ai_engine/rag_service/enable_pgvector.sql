7373f92634ca6b89f8a15782c3e6335b47b85320
hoahung2307 <angelnuyn@gmail.com>
2026-01-30 11:52:27 +0700

    Refactor embedding (ICD, Patient Infomation)

--- backend/apps/ai_engine/rag_service/enable_pgvector.sql
+++
@@ -1,8 +0,0 @@
--- Enable pgvector extension for PostgreSQL
--- Run this SQL script before running Django migrations
-
-CREATE EXTENSION IF NOT EXISTS vector;
-
--- Create index for faster vector similarity search
--- This will be created automatically by Django migrations, but listed here for reference
--- CREATE INDEX ON ai_engine_vectordocument USING ivfflat (embedding vector_cosine_ops) WITH (lists = 100);
